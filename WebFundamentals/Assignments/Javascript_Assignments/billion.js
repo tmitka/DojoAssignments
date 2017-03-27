ff1624e8dc53831331a2dc0697a6bf354324eddf
Ted Mitka <tmitka64@gmail.com>
2017-03-27 14:32:29 -0500

    created money adder will fix after today

---
+++ WebFundamentals/Assignments/Javascript_Assignments/billion.js
@@ -0,0 +1,25 @@
+function makemoney(number){
+    var sum = 0;
+    var pennies = 1;
+    var collected = 0;
+    var dollars = 0;
+    for (var i = 1; i <= number; i ++){
+        if (number == 1){
+            collected ++;
+            sum = sum + pennies;
+            pennies += 1;
+            
+        } else {
+            pennies = (collected * 2);
+            sum = sum + pennies
+            dollars = sum / 100
+            collected ++;
+           
+        }
+    }
+     console.log("You will have $", dollars, " after", number, "days.");
+    }
+
+
+
+makemoney(30)
