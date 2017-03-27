ee01c5fefa833b348c0d6bc4f6825ccd780bd925
Ted Mitka <tmitka64@gmail.com>
2017-03-27 11:50:10 -0500

    declare variables and finish task

---
+++ WebFundamentals/Assignments/Javascript_Assignments/dodeclare.js
@@ -0,0 +1,27 @@
+var FIRSTNUM = 42;
+var SECONDNUM = -12;
+var THIRDNUM = 1000;
+var FOURTHNUM = 1;
+
+var FIRSTSTRING = "Hello World!";
+var SECONDSTRING = "Blackhawks tickets on sale today";
+var THIRDSTRING = "How's the weather?";
+
+var LOGGEDIN = true;
+var TURNEDOFF = false;
+
+var UNDEF = undefined;
+
+var numstring = [FIRSTNUM, SECONDNUM, THIRDNUM, FOURTHNUM];
+var stringofstrings = [FIRSTSTRING, SECONDSTRING, THIRDSTRING];
+var booleans = [LOGGEDIN, TURNEDOFF];
+
+var undefarr = [UNDEF]
+
+
+console.log(numstring);
+console.log(stringofstrings);
+console.log(booleans);
+console.log(undefarr);
+
+
