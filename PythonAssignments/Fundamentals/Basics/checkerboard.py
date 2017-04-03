5618fcb139b1de022280202c30bc00be765c80db
Ted Mitka <tmitka64@gmail.com>
2017-04-03 14:12:34 -0500

    add functionality to comparr

--- PythonAssignments/Fundamentals/Basics/checkerboard.py
+++ PythonAssignments/Fundamentals/Basics/checkerboard.py
@@ -1,10 +1,13 @@
-evenlines = "* * * * "
-oddlines = " * * * *"
-i = 0
+#evenlines = "* * * * "
+#oddlines = " * * * *"
+#i = 0
+#
+#while i < 9:
+#    if (i % 2 == 0):
+#        print evenlines
+#    if (i % 2 != 0):
+#        print oddlines
+#    i = i + 1
 
-while i < 9:
-    if (i % 2 == 0):
-        print evenlines
-    if (i % 2 != 0):
-        print oddlines
-    i = i + 1
+def checkerboard():
+    d
--- PythonAssignments/Fundamentals/Basics/comparr.py
+++ PythonAssignments/Fundamentals/Basics/comparr.py
@@ -1,7 +1,15 @@
 list_one = [1,2,5,6,2]
-list_two = [1,2,5,6,2]
+list_two = [1,2,4,6,2]
 
-if list_one == list_two:
-    print "The lists are the same"
-else:
-    print "The lists are not the same"
+#if list_one == list_two:
+#    print "The lists are the same"
+#else:
+#    print "The lists are not the same"
+
+def comparr(list_one, list_two):
+    if list_one == list_two:
+        print "The lists are the same"
+    else:
+        print "The lists are not the same"
+
+comparr(list_one, list_two)
