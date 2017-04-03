571fccf8ab6820c49dc63d6678715a1968374a7a
Ted Mitka <tmitka64@gmail.com>
2017-04-03 14:43:21 -0500

    make checkerboard a function

--- PythonAssignments/Fundamentals/Basics/checkerboard.py
+++ PythonAssignments/Fundamentals/Basics/checkerboard.py
@@ -1,12 +1,12 @@
-#evenlines = "* * * * "
-#oddlines = " * * * *"
-#i = 0
-#
-#while i < 9:
-#    if (i % 2 == 0):
-#        print evenlines
-#    if (i % 2 != 0):
-#        print oddlines
-#    i = i + 1
-
 def checkerboard():
+    evenlines = "* * * * "
+    oddlines = " * * * *"
+    i = 0
+    while i < 9:
+        if (i % 2 == 0):
+            print evenlines
+        if (i % 2 != 0):
+            print oddlines
+        i = i + 1
+
+checkerboard()
