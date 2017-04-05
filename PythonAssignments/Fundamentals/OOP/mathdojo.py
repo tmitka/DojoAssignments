ee21f25e770cf39a3972e438ca0e0fedf566e6bc
Ted Mitka <tmitka64@gmail.com>
2017-04-05 16:01:03 -0500

    pushed a working part one with bad code commented out

--- PythonAssignments/Fundamentals/OOP/mathdojo.py
+++ PythonAssignments/Fundamentals/OOP/mathdojo.py
@@ -3,36 +3,36 @@ class MathDojo(object):
         self.sum = 0
 
     def add(self, *addnumber):
-        if type(addnumber) is tuple:
-            numberlist = list(addnumber)
-            for number in numberlist:
-                sumlist = []
-                sumlist.append(number)
-                print sumlist
+        #if type(addnumber) is tuple:
+            #numberlist = list(addnumber)
+            #for number in numberlist:
+                #sumlist = []
+                #sumlist.append(number)
+                #print sumlist
                 #self.sum = self.sum + number
-            return self
+            #return self
             #for number in addnumber:
             #    numberlist = []
             #    numberlist.append(number)  
             #    self.sum = self.sum + number
             #return self
 
-        if type(addnumber) is int:
+       # if type(addnumber) is int:
             for number in addnumber:
-                numberlist = []
-                numberlist.append(number)
+               # numberlist = []
+                #numberlist.append(number)
                 self.sum = self.sum + number
             return self
 
     def subtract(self, *subnumber):
-        if type(subnumber) is int:
+        #if type(subnumber) is int:
             for number in subnumber:
-                numberlist = []
-                numberlist.append(number)
+                #numberlist = []
+                #numberlist.append(number)
                 self.sum = self.sum - number
             return self
 
     def result(self):
         print self.sum
 
-MathDojo().add([5, 2, 3])
+MathDojo().add(2).add(2, 5, 3).subtract(3, 2).result()
