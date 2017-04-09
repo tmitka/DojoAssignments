c912acbe4e6899c971a3d2feaec5eb9b460c8fd2
Ted Mitka <tmitka64@gmail.com>
2017-04-09 17:03:00 -0500

    update dojosurvey to handle validation

--- PythonAssignments/Flask/dojosurvey/server.py
+++ PythonAssignments/Flask/dojosurvey/server.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
+app.secret_key = 'W9Zzyj$3h#4$ck6kEY'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/result', methods=['POST'])
+@app.route('/process', methods=['POST'])
 def form_submit():
-    name = request.form['name']
-    location = request.form['location']
-    language = request.form['language']
-    comment = request.form['comment']
-
-    return render_template('info.html', name=name, location=location, language=language, comment=comment)
+    if len(request.form['name']) < 1:
+        flash('Name cannot be empty')
+    else:
+        flash('Success')
+    if len(request.form['comment']) < 1:
+        flash('please enter a comment')
+    elif len(request.form['comment']) > 120:
+        flash('comment cannot be longer than 120 characters')
+    return redirect('/')
 
 app.run(debug=True)
