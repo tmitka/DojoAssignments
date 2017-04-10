8956993a3ed67cc2d99791b1cbb764a0c93a35b6
Ted Mitka <tmitka64@gmail.com>
2017-04-10 17:52:22 -0500

    do great number game

--- PythonAssignments/Flask/numbergame/server.py
+++ PythonAssignments/Flask/numbergame/server.py
@@ -6,14 +6,30 @@ app.secret_key = 'mitka'
 
 @app.route('/')
 def index():
-    winningnumber = random.randrange(0, 101)
-    if 'winningnumber' in session:
-        print session['winningnumber']
-    else:
-        session['winningnumber'] = winningnumber
-        print session['winningnumber']
+    if 'winningnumber' not in session.keys():
+        session['winningnumber'] = random.randrange(0, 101)
+        
+    if 'guess' in session.keys():
+        print "Your guess is", session['guess']
+        if session['guess'] > session['winningnumber']:
+            print "Your guess: {} > the winning number:{}".format(session['guess'], session['winningnumber'])
+            session['answer'] = "Too high"
+    
+        elif session['guess'] < session['winningnumber']:
+            print "Your guess: {} < the winning number {}".format(session['guess'], session['winningnumber'])
+            session['answer'] = "Too low"
+        else: 
+            print "session guress: {} is right".format(session['guess'])
+            session['answer'] = "You got it" 
 
     return render_template('index.html')
+
+@app.route('/guess', methods=["POST"])
+def guess():
+    print 'Guess is'
+    session['guess'] = int(request.form['guess'])
+    print session['guess']
+    return redirect('/')
  
 
 app.run(debug=True)
