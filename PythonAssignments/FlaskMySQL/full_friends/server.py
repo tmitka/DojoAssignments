b9eb14a1e9b4045a8e25ae288d6d9dd6691799c7
Ted Mitka <tmitka64@gmail.com>
2017-04-12 14:35:26 -0500

    do friends

---
+++ PythonAssignments/FlaskMySQL/full_friends/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+mysql = MySQLConnector(app,'full_friends')
+@app.route('/')
+def index():
+    query = "SELECT * FROM friends"
+    friends = mysql.query_db(query)
+    return render_template('index.html', all_friends=friends)
+    
+@app.route('/friends', methods=['POST'])
+def create():
+    # Write query as a string. Notice how we have multiple values
+    # we want to insert into our query.
+    query = "INSERT INTO friends (name, age, friend_since, year) VALUES (:name, :age, NOW(), NOW())"
+    # We'll then create a dictionary of data from the POST data received.
+    data = {
+             'name': request.form['name'],
+             'age':  request.form['age'],
+
+           }
+    # Run query, with dictionary values injected into the query.
+    mysql.query_db(query, data)
+    return redirect('/')
+app.run(debug=True)
