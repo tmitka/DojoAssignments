2d426a04617bfc424b1569f163f51d672aa141a3
Ted Mitka <tmitka64@gmail.com>
2017-05-09 11:42:44 -0500

    mathdojo and project

---
+++ Ruby/first_vagrant_box/mathdojo.rb
@@ -0,0 +1,19 @@
+class MathDojo
+    attr_reader :result
+    def initialize
+        @result = 0
+    end
+
+    def add(*numbers)
+        @result += numbers.flatten.reduce(0, :+)
+        self
+    end
+
+    def subtract(*numbers)
+        @result -= numbers.flatten.reduce(0, :+)
+        self
+    end
+end
+
+puts challenge1 = MathDojo.new.add(2).add(2, 5).subtract(3, 2).result
+puts challenge2 = MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract([2,3], [1.1, 2.3]).result
