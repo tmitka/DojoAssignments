cf5d1d9f19c3fc296bf82c4dd228c3b5c36f0c02
Ted Mitka <tmitka64@gmail.com>
2017-05-10 18:16:30 -0500

    dojo ninjas

---
+++ Ruby/first_vagrant_box/dojo_ninjas/db/migrate/20170510225357_create_ninjas.rb
@@ -0,0 +1,12 @@
+class CreateNinjas < ActiveRecord::Migration
+  def change
+    create_table :ninjas do |t|
+      t.string :first_name
+      t.string :string
+      t.string :last_name
+      t.references :dojo, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
