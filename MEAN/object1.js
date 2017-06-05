8ee639cc53603162cd4dafaa9d3afd8faff522f0
Ted Mitka <tmitka64@gmail.com>
2017-06-05 18:23:36 -0500

    object 2

--- MEAN/object1.js
+++ MEAN/object1.js
@@ -1,32 +1,44 @@
-function VehicleConstructor(name, wheels, passengers){
-    var vehicle = {};
-
-    vehicle.name = name;
-    vehicle.wheels = wheels;
-    vehicle.passengers = passengers;
-    vehicle.makeNoise = function(noise){
+function VehicleConstructor(name, wheels, passengers, speed){
+    var distanceTraveled = 0;
+    var self = this;
+    this.Traveled = function(){
+        distanceTraveled += self.speed;
+        console.log(distanceTraveled);
+    }
+    this.name = name;
+    this.wheels = wheels;
+    this.passengers = passengers;
+    this.speed = speed;
+    this.makeNoise = function(noise){
         console.log(noise)
     }
-    return vehicle
+    this.move = function(){
+        this.makeNoise()
+        Traveled()
+    }
+    this.miles = function(){
+        console.log(distanceTraveled);
+    }
 }
 
-var bike = VehicleConstructor("Bike", 2, 1)
+var bike =  new VehicleConstructor("Bike", 2, 1, 15)
 
-//console.log(bike.name)
+bike.name
+bike.move
+bike.Traveled()
+bike.Traveled()
+console.log(bike.miles)
 //console.log(bike.wheels)
 //console.log(bike.passengers)
 //bike.makeNoise("Ring Ring")
 
-var sedan = VehicleConstructor("Sedan", 4, 5)
+var sedan =  new VehicleConstructor("Sedan", 4, 5)
 //sedan.makeNoise("Honk Honk")
 
-var bus = VehicleConstructor("Bus", 4, 20)
+var bus = new VehicleConstructor("Bus", 4, 20)
 bus.passengerNumber = 0
 bus.pickupPassengers = function(newPassengers){
     bus.passengerNumber += newPassengers;
 }
-
-bus.pickupPassengers(10)
-bus.pickupPassengers(10)
 
 console.log(bus.passengerNumber)
