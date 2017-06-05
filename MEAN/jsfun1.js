bc3bf1512da8f464208b46d5b7ac680d706577ef
Ted Mitka <tmitka64@gmail.com>
2017-06-05 12:39:17 -0500

    js fundamentals 1

---
+++ MEAN/jsfun1.js
@@ -0,0 +1,56 @@
+//var x = [3,5,"Dojo", "rocks", "Michael", "Sensei"]
+
+//for (var i = 0; i < x.length; i+= 1){
+//    console.log(x[i]);
+//}
+// console log the array
+
+//x.push(100)
+// add 100
+
+//x.push(["hello", "world", "js is fun"])
+
+//console.log(x)
+// add more and console log
+
+//function sum(number){
+//    var new_sum = 0
+//    for (i = 0; i <= number; i += 1){
+//        new_sum = new_sum + i;
+//    }
+//    console.log(new_sum)
+//}
+
+//sum(500)
+// can sum any number
+
+//var array = [1, 5, 90, 25, -3, 0]
+//var min = 0;
+//for (i = 0; i < array.length; i += 1){
+//    if (array[i] < min){
+//        min = array[i]
+//    }
+//}
+//console.log(min)
+// min value array
+
+//var array = [1, 5, 90, 25, -3, 0]
+//var avg = 0
+//var sum = 0
+//for (i = 0; i < array.length; i += 1){
+//    sum = sum + array[i]
+//    avg = sum/array.length
+//}
+//console.log(avg)
+// average
+//var newNinja = {
+// name: 'Jessica',
+//profession: 'coder',
+// favorite_language: 'JavaScript', //like that's even a question!
+// dojo: 'Dallas'
+//}
+
+//for (key in newNinja){
+//    console.log(key, newNinja[key])
+//}
+//console log the key value pairs
--- MEAN/test.js
+++ MEAN/test.js
@@ -1,1 +1,30 @@
-console.log("I am running from node");
+var glazedDonuts = [
+  {
+    frosting: 'glazed',
+    type: 'old fashioned',
+    age: '45',
+    time: 'minutes'
+  },
+  {
+    frosting: 'glazed',
+    type: 'regular',
+    age: '5',
+    time: 'minutes'
+  },
+  {
+    frosting: 'glazed',
+    type: 'jelly filled',
+    age: '1',
+    time: 'seconds'
+  }
+];
+
+var purchase;
+//You
+if((glazedDonuts[0].age < 25 && glazedDonuts[0].time == 'minutes') || glazedDonuts[0].time == 'seconds'){
+  //shop owner
+  purchase = glazedDonuts[0];
+}
+else {
+  console.log('sorry it has been out a bit longer');
+}
