dc24b129e40e090346e62e0f4b896c6674e1de11
Ted Mitka <tmitka64@gmail.com>
2017-09-05 13:56:04 -0500

    boxing

---
+++ C#/boxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> objlist = new List<object>();
+            int sum = 0;
+            objlist.Add(7);
+            objlist.Add(28);
+            objlist.Add(-1);
+            objlist.Add(true);
+            objlist.Add("chair");
+            for (var i = 0; i < objlist.Count; i += 1){
+                Console.WriteLine(objlist[i]);
+                if (objlist[i] is int){
+                    int value = System.Convert.ToInt32(objlist[i]);
+                    sum += value;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
